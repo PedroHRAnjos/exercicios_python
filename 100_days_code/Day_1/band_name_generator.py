d113a3b0723586961e9153dd4f72a773b35375a1
PedroHRAnjos <pedroh.rodriguesanjos@gmail.com>
2025-11-10 14:37:29 -0300

    Projeto day 1 - Gerador de nomes de bandas

---
+++ 100_days_code/Day_1/band_name_generator.py
@@ -0,0 +1,11 @@
+# Projeto 100 Days of Code - Dia 1
+
+# Apresentação do programa
+print("Bem-vindo ao gerador de nomes de bandas!")
+
+# Coleta de informações do usuário
+cidade = input("Qual o nome da cidade que você nasceu?\n")
+pet = input("Qual o nome do seu pet?\n")
+
+# Geração do nome da banda
+print("O nome da sua banda é: " + cidade + " " + pet)
